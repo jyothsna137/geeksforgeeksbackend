44633af19544ce5910611a6a61ec4572e7aab1de
jyothsna137 <96375763+jyothsna137@users.noreply.github.com>
2022-01-22 12:30:41 +0530

    first commit

---
+++ simpleExpress.ts
@@ -0,0 +1,14 @@
+import * as express from "express"
+
+const app = express();
+
+const router = express.Router();
+router.get("/a", (request, respose) => respose.send("api a is invoked"));
+
+app.use(router);
+
+app.listen(4000, () => console.log("simple express sever is created"));
+
+/**
+ * express -  router is inbuilt
+ */
---
+++ simpleKoa.ts
@@ -0,0 +1,12 @@
+import * as koa from "koa";
+import * as Router from "koa-router"
+
+const app = new koa();
+
+const router = new Router();
+
+router.get("/a", (context) => context.response.body = ("api a is invoked"));
+
+app.use(router.routes());
+
+app.listen(4000, () => console.log("simple koa server is created"));
